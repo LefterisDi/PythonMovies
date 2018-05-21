d50cf765334e4604982937a2e2e428c14f8f7549
MichaelXanth <xan.michael98@yahoo.com>
2018-05-21 14:19:18 +0300

    changes

--- application/app.py
+++ application/app.py
@@ -24,15 +24,16 @@ def updateRank(rank1, rank2, movieTitle):
     try:
         float(rank1)
         if float(rank1) > 10.0 or float(rank1) < 0.0:
-            return [("status",),("error",),]
+            return [("Status",),("Error",),]
     except ValueError:
-        return [("status",),("error",),]
+        return [("Status",),("Error",),]
+
     try:
         float(rank2)
         if float(rank2) > 10.0 or float(rank2) < 0.0:
-            return [("status",),("error",),]
+            return [("Status",),("Error",),]
     except ValueError:
-        return [("status",),("error",),]
+        return [("Status",),("Error",),]
 
     sql = "SELECT title , rank FROM movie;"
     cur.execute(sql)
@@ -42,12 +43,12 @@ def updateRank(rank1, rank2, movieTitle):
     for row in cur.fetchall():
         if row[0] == movieTitle and unique == 1:
             if unique == 0:
-                return [("status",),("error",),]
+                return [("Status",),("Error",),]
             unique = 0
             movRank = row[1]
 
     if unique == 1:
-        return [("status",),("error",),]
+        return [("Status",),("Error",),]
 
     if movRank is None:
         newRank = (float(rank1) + float(rank2)) / 2.0
@@ -67,7 +68,7 @@ def updateRank(rank1, rank2, movieTitle):
 
     print (rank1, rank2, movieTitle)
 
-    return [("status",),("ok",),]
+    return [("Status",),("Ok!",),]
 
 
 
@@ -76,15 +77,62 @@ def updateRank(rank1, rank2, movieTitle):
 def colleaguesOfColleagues(actorId1, actorId2):
 
     # Create a new connection
-    con=connection()
+    con = connection()
 
     # Create a cursor on the connection
-    cur=con.cursor()
+    cur = con.cursor()
+
+    try:
+        int(actorId1)
+    except ValueError:
+        return [("Status",),("Error",),]
+
+    try:
+        int(actorId2)
+    except ValueError:
+        return [("Status",),("Error",),]
+
+    list = [("Movie Title" , "Colleague of Actor_1" , "Colleague of Actor_2" , "Actor 1" , "Actor 2"),]
+
+
+    sql = """
+              SELECT DISTINCT mv.title , rl1.actor_id , rl2.actor_id , %s , %s
+
+              FROM movie mv , role rl1 , role rl2
+
+              WHERE     rl1.actor_id < rl2.actor_id
+             	    AND rl1.movie_id = rl2.movie_id
+                    AND rl1.actor_id <> %s
+                    AND rl1.actor_id <> %s
+                    AND rl2.actor_id <> %s
+                    AND rl2.actor_id <> %s
+
+            	    AND EXISTS(SELECT DISTINCT rl3.movie_id , rl4.movie_id
+
+                               FROM role nrl1 , role nrl2 , role rl3 , role rl4
+
+                               WHERE     rl3.actor_id = %s
+            					     AND rl4.actor_id = %s
+                                     AND nrl1.actor_id = rl1.actor_id
+                                     AND nrl2.actor_id = rl2.actor_id
+                                     AND rl3.movie_id = nrl1.movie_id
+                                     AND rl4.movie_id = nrl2.movie_id
+                              )
+
+                    AND rl1.movie_id = mv.movie_id
+
+              ORDER BY rl1.actor_id , rl2.actor_id;
+
+          """ % (actorId1 , actorId2 , actorId1 , actorId2 , actorId1 , actorId2 , actorId1 , actorId2)
+
+    cur.execute(sql)
+
+    for i in cur:
+        list.append(i)
 
     print (actorId1, actorId2)
 
-    return [("movieTitle", "colleagueOfActor1", "colleagueOfActor2", "actor1","actor2",),]
-
+    return list;
 
 
 
